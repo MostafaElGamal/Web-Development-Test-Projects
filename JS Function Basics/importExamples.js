a4fdd7135f3182ef95f6e0edfeaf70d7f29692b4
Mostafa <30895073+MostafaElGamal@users.noreply.github.com>
2019-11-03 21:06:32 +0200

    Create importExamples.js

---
+++ JS Function Basics/importExamples.js
@@ -0,0 +1,9 @@
+// This is how you import a file the 'greet' can be any name
+import greet from './exportExamples'
+
+// You can also export multiple items from a file check exportExamples for more reference
+import name from './exportExamples'
+import age from './exportExamples'
+import favColour from './exportExamples'
+
+import {name, age} from './exportExamples'
